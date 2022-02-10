d2e0346f6140240fb6741111be3563e771be1d85
gyf <405126907@qq.com>
2022-02-10 14:27:35 +0800

    1.使用QtDPi设置

--- CustomControls/main.cpp
+++ CustomControls/main.cpp
@@ -1,8 +1,23 @@
 #include "CustomControls.h"
 #include <QtWidgets/QApplication>
 #pragma execution_character_set("utf-8")
+
+//使用Qt内置高DPI处理方式
+void QtDpiSetting()
+{
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+	qputenv("QT_ENABLE_HIGHDPI_SCALING", "1");
+	// 5.14.0
+#if QT_VERSION > 0x050E00
+	QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
+}
+
 int main(int argc, char *argv[])
 {
+	//在QApplication构造前设置
+	QtDpiSetting();
 	QApplication a(argc, argv);
 	CustomControls w;
 	w.show();
